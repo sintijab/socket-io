54c0d051d58c04b3959ebb288f754b4949f9c642
Sintija Birgele <sintijabirgele@Sintijas-Laptop.local>
2024-12-29 05:27:02 +0100

    fix: cors

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const server = http.createServer(app);
 import { Server } from "socket.io";
 const io = new Server(server, {
   cors: {
-    origin: 'http://localhost:3000',
+    origin: 'https://cofun.digital',
     methods: ["GET", "POST"],
   }
 });
